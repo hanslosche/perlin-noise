dd9546dd5d1a306568f360395a8f8ccd9718948c
Hans <hansloesche.art@gmail.com>
2020-10-15 16:23:15 -0400

    updated master

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+#include "image.hpp"
+#include "perlin.hpp"
+
 int main() {
-	std::cout << "Hello, Ken!" << std::endl;
+	std::cout << "Hello, o!" << std::endl;
+
+
+	perlin::Perlin map(1532512342);
+
+	Image image;
+	image.generate(512, 512);
+
+	double frequency = 8;
+	double fx = image.getWidth() / frequency;
+	double fy = image.getHeight() / frequency;
+
+
+	for (int y = 0; y < 512; y++) {
+		for (int x = 0; x < 512; x++) {
+			double p = map.noise2D(x / fx, y / fy);
+		}
+	}
+
+	image.write("out.bmp");
 	return 0;
 }
